2678d08367ea30c898177eb5f393b220dff6a759
rbsingh001 <robinhooda25@gmail.com>
2023-09-12 20:29:18 +0530

    background color and boldList

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var headerTitle = document.getElementById('header-title');
-// headerTitle.style.border = 'solid 2px black';
+
 var header = document.getElementById('main-header');
 
 header.style.border = 'solid 2px black';
@@ -7,3 +7,11 @@ header.style.border = 'solid 2px black';
 var additem = document.getElementById('additem');
 additem.innerHTML = '<b><h2  class="title">Add Items</h2></b>';
 additem.style.color = 'green';
+
+var items = document.getElementsByClassName('list-group-item');
+
+items[2].style.backgroundColor = 'green'
+
+for(let i=0; i<items.length; i++){
+    items[i].style.fontWeight = 'bold';
+}
