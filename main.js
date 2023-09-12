f2c26e0c92aa618a6b2f57ae7786c3ebe634ad97
rbsingh001 <robinhooda25@gmail.com>
2023-09-12 19:56:50 +0530

    html commit

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('first-git');
+var headerTitle = document.getElementById('header-title');
+// headerTitle.style.border = 'solid 2px black';
+var header = document.getElementById('main-header');
+
+header.style.border = 'solid 2px black';
+
+var additem = document.getElementById('additem');
+additem.innerHTML = '<b><h2  class="title">Add Items</h2></b>';
+additem.style.color = 'green';
